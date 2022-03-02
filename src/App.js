655851400524a99ac73a8459ad40c90274949d67
nou-ali <noura4403@email.com>
2022-03-02 18:38:53 -0500

    search function is now working

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-          <ImageSearch/>
+          <ImageSearch searchText={(text) => setTerm(text)}/>
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6 ">
           {images.map((image) => (
--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-const ImageSearch = () => {
+const ImageSearch = ({searchText}) => {
+  const [text, setText] = useState("");
+  const onSubmit = (e) => {
+      e.preventDefault();
+      searchText(text);
+  }
+
+
   return (
     <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
-      <form className="w-full max-w-sm">
+      <form onSubmit={onSubmit}
+      className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-pink-500 py-2">
-        <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="What are you looking for?" />
+        <input onChange={e => setText(e.target.value)}
+        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="What are you looking for?" />
         <button className="flex-shrink-0 bg-pink-500 hover:bg-pink-700 border-pink-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
       Search
       </button>
